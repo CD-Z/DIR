dfca35094dafa7c40131bf6208e6c465e54b0f1d
Patrick Loser <github.verify097@passmail.com>
2026-05-15 17:05:45 +0200

    Update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
-    // add yur domain name here
-   site: 'https://yoursite.com',
-  integrations: [sitemap()]
+  // add yur domain name here
+  site: "https://cd-z.github.io/DIR/",
+  integrations: [sitemap()],
 });
